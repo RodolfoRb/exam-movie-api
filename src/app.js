3ea3733082cedf346c3f9480fbcfed337d468356
Rodolfo Rebollo Burgueño <rodolfo.rb@live.com>
2021-03-02 23:23:28 -0600

    jest and swagger configuration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const swaggerUi = require("swagger-ui-express");
 
 const movieRoutes = require("./routes/movies.routes");
 const authRoutes = require("./routes/auth.routes");
+
+const swaggerDoc = require("./swagger.json");
 
 const app = new express();
 app.set("port", process.env.PORT || 4000);
@@ -13,5 +16,15 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/movies", movieRoutes);
 app.use("/api/auth", authRoutes);
+
+var options = {
+  explorer: true,
+  swaggerOptions: {
+    "apis": [
+      "./auth.routes.js"
+    ]
+  }
+};
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc, options));
 
 module.exports = app;
